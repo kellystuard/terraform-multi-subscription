105bff6629540f30a9191aeb29d260d8ded30d56
Kelly Stuard <kellystuard@gmail.com>
2020-01-07 18:51:27 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,19 +6,19 @@ provider "azurerm" {
 }
 #resource "azurerm_resource_group" "main" {
 #  name     = "terraform-multi-subscription"
-#  location = var.location
+#  location = "North Central US"
 #}
 resource "azurerm_resource_group" "main1" {
-  name     = "terraform-multi-subscription"
-  location = var.location
+  name     = "terraform-multi-subscription1"
+  location = "North Central US"
   
   providers = {
     azurerm = azurerm.azure1
   }
 }
 resource "azurerm_resource_group" "main2" {
-  name     = "terraform-multi-subscription"
-  location = var.location
+  name     = "terraform-multi-subscription2"
+  location = "North Central US"
   
   providers = {
     azurerm = azurerm.azure2
